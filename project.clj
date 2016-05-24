ada02dc184a5b2028aead932ad813e7c54a15d5d
Andrew Monks <a@monks.co>
2016-05-24 16:13:07 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.taoensso/timbre "4.0.1"]
-                 [yeller-clojure-client "1.2.1"]])
+                 [yeller-clojure-client "1.4.3"]])
